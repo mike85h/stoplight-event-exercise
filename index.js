f044ddc544a15977c1c769c046d9c2ffe46d8fe5
Michael Harrington <mike85h@gmail.com>
2017-06-14 14:24:28 -0600

    first part finished

--- index.js
+++ index.js
@@ -1,5 +1,45 @@
 (function() {
   'use strict';
+  document.getElementById('stopButton').onclick = illuminateRed;
+  document.getElementById('slowButton').onclick = illuminateYellow;
+  document.getElementById('goButton').onclick = illuminateGreen;
+
+  function illuminateRed() {
+    if(document.getElementById('stopLight').style.backgroundColor == "red"){
+      document.getElementById('stopLight').style.backgroundColor = "black";
+    }else{
+      document.getElementById('stopLight').style.backgroundColor = "red";
+    }
+  }
+
+  function illuminateYellow() {
+    if(document.getElementById('slowLight').style.backgroundColor == "yellow"){
+      document.getElementById('slowLight').style.backgroundColor = " black";
+    }else{
+      document.getElementById('slowLight').style.backgroundColor = "yellow";
+    }
+  }
+
+  function illuminateGreen(){
+    if(document.getElementById('goLight').style.backgroundColor == "green"){
+      document.getElementById('goLight').style.backgroundColor = "black";
+    }else{
+      document.getElementById('goLight').style.backgroundColor = "green";
+    }
+  }
+
+  //if .mouseenter event happens, then console.log("Entered <textContent> Button") .mouseleave
+  //
+
 
   // YOUR CODE HERE
 })();
+
+
+
+
+// function clearLights() {
+//   document.getElementById('stopLight').style.backgroundColor = "black";
+//   document.getElementById('slowLight').style.backgroundColor = "black";
+//   document.getElementById('goLight').style.backgroundColor = "black";
+// }
